36f5220ff201a111f2dfb20e4ab4311b0b4e6a99
adn974 <56817528+adnan974@users.noreply.github.com>
2020-08-06 10:13:06 +0400

    ajout des fonctions qui permettent le mouvement du serpent avec les touches directionnelles

--- game.js
+++ game.js
@@ -34,28 +34,98 @@ function Snake() {
     this.color = "red";
     this.x = 0;
     this.y = 0;
-    this.xSpeed = 5;
+    this.newX = 5;
+    this.newY = this.y;
     this.width = 10;
     this.height = 10;
 
+    // TODO: setter (?)
+    this.selectColor = function (color) {
+        this.canvasContext.fillStyle = color;
+        this.color = color;
+    }
+
     //TODO: Définir cette fonction à l'aide d'un prototype ?
-    this.draw = function () {
-        this.canvasContext.fillStyle = this.color;
+    this.drawInit = function () {
         this.canvasContext.fillRect(this.x, this.y, this.width, this.height);
     };
-    this.clear = function(){
+
+    this.newDraw = function () {
+        this.canvasContext.fillRect(this.newX, this.newY, this.width, this.height);
+    }
+
+    this.clear = function () {
         this.canvasContext.clearRect(this.x, this.y, this.width, this.height);
-        
+
     }
-    this.moove = function(){
+
+    this.mooveDirection = function (direction) {
+        switch (direction) {
+            case "right":
+                this.newX += 5;
+                this.x = this.newX;
+                break;
+
+            case "left":
+                this.newX -= 5;
+                this.x = this.newX;
+                break;
+
+            case "up":
+                this.newY -=5;
+                this.y = this.newY;
+
+                break;
+
+            case "down":
+                this.newY +=5;
+                this.y = this.newY;
+
+                break;
+            }
+        this.newDraw();
+        
+
+    }
+
+
+    this.mooveInit = function () {
+
+        const asciiMooveLeftCode = 37;
+        const asciiMooveRightCode = 39;
+        const asciiMooveUpCode = 38;
+        const asciiMooveDownCode = 40;
+
+        let direction = "right";
+
         setInterval(() => {
-            this.canvasContext.fillRect(this.xSpeed, this.y, this.width, this.height);
-            this.x = this.xSpeed;
-            this.xSpeed += 5;
-        }, 500); 
-        
-        
+
+            this.clear();
+            this.mooveDirection(direction);
+            
+
+            document.addEventListener('keydown', function (e) {
+                console.log(e.keyCode);
+                switch (e.keyCode) {
+                    case asciiMooveLeftCode:
+                        direction = "left";
+                        break;
+                    case asciiMooveRightCode:
+                        direction = "right";
+                        break;
+                    case asciiMooveUpCode:
+                        direction = "up";
+                        break;
+                    case asciiMooveDownCode:
+                        direction = "down";
+                        break;
+                }
+
+            })
+        }, 500);
+
     }
+
 }
 
 
@@ -64,11 +134,14 @@ function Snake() {
     var snake = new Snake();
 
     boardGame.create();
-    snake.draw();
+    snake.selectColor("red");
+    snake.drawInit();
 
-    snake.moove();
+    snake.mooveInit();
 
 })()
+
+
 
 
 
