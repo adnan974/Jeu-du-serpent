1f0fc8b8ab373cd1ca8e6d93068abe79156aec82
adn974 <56817528+adnan974@users.noreply.github.com>
2020-08-09 14:37:52 +0400

    ajout d un systeme de colision et d une apparition aleatoire des fruits

--- game.js
+++ game.js
@@ -19,14 +19,14 @@ function BoardGame() {
     // nouvelle version
     // Pq ce changement: principe du clean code(?), ajouter "board" crée de la redondance
     this.color = "black";
-    this.height = 500;
-    this.width = 500;
+    this.height = 300;
+    this.width = 300;
     this.borderWitdh = "2";
 
     this.create = function () {
         this.canvasContext.lineWidth = this.borderWitdh;
         this.canvasContext.fillStyle = this.color;
-        this.canvasContext.strokeRect(0,0, this.height, this.width);
+        this.canvasContext.strokeRect(0, 0, this.height, this.width);
     }
 }
 
@@ -34,8 +34,8 @@ function Snake() {
     Canvas.call(this);
     this.color = "red";
 
-    this.newX = 0;
-    this.newY = 0;
+    this.newX = 5;
+    this.newY = 5;
 
     this.width = 10;
     this.height = 10;
@@ -43,6 +43,7 @@ function Snake() {
     this.growthNumber = 5;
 
     this.direction = "right";
+    this.previousDirection = null;
 
     this.arrayBody = [[this.newX, this.newY]];
 
@@ -54,7 +55,7 @@ function Snake() {
         this.color = color;
     }
 
- 
+
 
     this.draw = function () {
         this.arrayBody.forEach(element => {
@@ -70,29 +71,57 @@ function Snake() {
     }
 
     this.mooveDirection = function () {
+
         switch (this.direction) {
             case "right":
-                this.newX += 5;
+                if (this.previousDirection == "left") {
+                    this.direction = "left";
+                    this.newX -= 5;
+                }
+                else {
+                    this.newX += 5;
+                }
                 break;
 
             case "left":
-                this.newX -= 5;
+                if (this.previousDirection == "right") {
+                    this.direction = "right";
+                    this.newX += 5;
+
+                }
+                else {
+                    this.newX -= 5;
+                }
                 break;
 
             case "up":
-                this.newY -= 5;
-                break;
+                if (this.previousDirection == "down") {
+                    this.direction = "down";
+                    this.newY += 5;
 
+                }
+                else {
+                    this.newY -= 5;
+                }
+                break;
             case "down":
-                this.newY += 5;
+                if (this.previousDirection == "up") {
+                    this.direction = "up";
+                    this.newY -= 5;
+
+                }
+                else {
+                    this.newY += 5;
+                }
                 break;
         }
+        this.previousDirection = this.direction;
         this.arrayBody.push([this.newX, this.newY]);
     }
 
 
 
-    this.listenEvent = function(){
+    this.listenEvent = function () {
         const asciiMooveLeftCode = 37;
         const asciiMooveRightCode = 39;
         const asciiMooveUpCode = 38;
@@ -118,10 +147,10 @@ function Snake() {
     }
 
     this.grow = function () {
-            this.growthNumber++;
-            this.arrayBody.push([null, null]);
-            console.log(this.arrayBody.length);
-        
+        this.growthNumber++;
+        this.arrayBody.push([null, null]);
+        console.log(this.arrayBody.length);
+
     }
 
 
@@ -129,26 +158,53 @@ function Snake() {
 
 }
 
-function Fruit(){
+function Fruit() {
     Canvas.call(this);
     this.color = "black";
     this.width = 10;
     this.height = 10;
-    this.x = 50;
-    this.y= 50;
+    this.x = 0;
+    this.y = 0;
+    this.boardGame = new BoardGame();
 
     this.selectColorF = function (color) {
         this.canvasContext.fillStyle = color;
         this.color = color;
     }
 
-    this.create = function(){
-        this.canvasContext.fillRect(this.x,this.y,this.width,this.height);
+    this.create = function () {
+        this.x = getRandomInt(5, this.boardGame.width - 5);
+        this.x -= this.x % 5;
+        this.y = getRandomInt(5, this.boardGame.height - 5);
+        this.y -= this.y % 5;
+        this.canvasContext.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-function Physics(){
+function Physics(snake,boardGame,fruit) {
 
+    this.snake = snake;
+    this.boardGame = boardGame;
+    this.fruit = fruit;
+
+    this.checkBoardColision = function(){
+        if((snake.newX==0 || snake.newY == 0)||snake.newX == boardGame.width||snake.newY == boardGame.height){
+            console.log("GAME OVER");
+            return true;
+        }
+    this.checkSnakeColision = function(){
+        
+    }
+        
+    }
+
+}
+
+// TODO: A ranger ?
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
@@ -156,7 +212,10 @@ function Physics(){
     var boardGame = new BoardGame();
     var snake = new Snake();
     var fruit = new Fruit();
-    
+
+    // A analyse: des lecons interessante à en tirer dans la maniere dont j'ai crée cette clase.
+    var physicsGame = new Physics(snake,boardGame,fruit);
+
 
 
     boardGame.create();
@@ -165,20 +224,9 @@ function Physics(){
 
     fruit.selectColorF("blue");
     fruit.create();
+
+
     setInterval(() => {
-        
-       // snake.listenEvent();
-       snake.clearBody();
-        snake.mooveDirection();
-        snake.selectColor('red');
-        snake.draw();
-        console.log("f.x= "+fruit.x+"f.y= "+fruit.y+"s.x= "+snake.newX+"s.y= "+snake.newY);
-         if (fruit.x == snake.newX && fruit.y == snake.newY){
-             snake.grow();
-             fruit.x += 20;
-             fruit.y += 20;
-             fruit.create();
-         }
 
         const asciiMooveLeftCode = 37;
         const asciiMooveRightCode = 39;
@@ -201,16 +249,30 @@ function Physics(){
                     break;
             }
 
+
         })
 
-    }, 100);
+        // snake.listenEvent();
+        physicsGame.checkBoardColision();
+        snake.clearBody();
+        snake.mooveDirection();
+        snake.selectColor('red');
+        snake.draw();
+        if (fruit.x == snake.newX && fruit.y == snake.newY) {
+            snake.grow();
+            fruit.create();
+        }
 
-    
+        
 
-   /* snake.mooveInit();
+    }, 50);
 
-    fruit.selectColor("blue");
-    fruit.initFruit();*/
+
+
+    /* snake.mooveInit();
+ 
+     fruit.selectColor("blue");
+     fruit.initFruit();*/
 
 
 
