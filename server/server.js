e9adba4426d583bb3b8882894914b3449102da4d
Adnan <adnan.goulamaly@gmail.com>
2020-08-26 16:59:54 +0400

    ajout d'une partie serveur avec node js. initialisation des modules importants (socket, express, ejs)

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+app.get("/",function(req,res){
+    res.render("index.ejs");
+})
+
+io.sockets.on('connection', function (socket) {
+
+})
+
+server.listen(8080);
